0a781c914b1af4eeef291568d3154caee6511118
marlonchiu <Knight1898@outlook.com>
2019-04-12 15:37:40 +0800

    table render JSX语法 自定义渲染

--- src/components/edit-table/edit-table.vue
+++ src/components/edit-table/edit-table.vue
@@ -1,9 +1,15 @@
 <template>
-  <Table :columns="columns" :data="data"></Table>
+  <Table :columns="insideColumns" :data="data"></Table>
 </template>
 <script>
 export default {
   name: 'EditTable',
+  data () {
+    return {
+      insideColumns: [], // 自定义列
+      editingId: ''
+    }
+  },
   props: {
     columns: {
       type: Array,
@@ -12,6 +18,31 @@ export default {
     data: {
       type: Array,
       default: () => []
+    }
+  },
+  mounted () {
+    const insideColumns = this.columns.map(item => {
+      // 编辑判断说明 -- 如果有render属性表示可编辑，如果item.editable为true则可编辑
+      // columns 和 data 需同时声明，声明后将导出指定的数据，建议列数据有自定义render时，可以根据需求自定义导出内容
+      if (!item.render && item.editable) {
+        // 处理逻辑 JSX语法
+        item.render = (h, { row, index, column }) => {
+          // console.log(row, index, column)
+          return (
+            <div>
+              {row[column.key]}
+              <i-button on-click={this.handleClick.bind(this, { row, index, column })}>编辑</i-button>
+            </div>
+          )
+        }
+        return item
+      } else return item
+    })
+    this.insideColumns = insideColumns
+  },
+  methods: {
+    handleClick ({ row, index, column }) {
+      console.log({ row, index, column })
     }
   }
 }
--- study_md/chapter17-19.md
+++ study_md/chapter17-19.md
@@ -46,5 +46,56 @@ import { getTableData } from '@/api/data'
 ```
 
 1）JSX进阶
+
+```javascript
+// 自定义渲染表格的方法
+// edit-table.vue
+
+export default {
+  name: 'EditTable',
+  data () {
+    return {
+      insideColumns: [], // 自定义列
+      editingId: ''
+    }
+  },
+  props: {
+    columns: {
+      type: Array,
+      default: () => []
+    },
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  mounted () {
+    const insideColumns = this.columns.map(item => {
+      // 编辑判断说明 -- 如果有render属性表示可编辑，如果item.editable为true则可编辑
+      // columns 和 data 需同时声明，声明后将导出指定的数据，建议列数据有自定义render时，可以根据需求自定义导出内容
+      if (!item.render && item.editable) {
+        // 处理逻辑 JSX语法
+        item.render = (h, { row, index, column }) => {
+          // console.log(row, index, column)
+          return (
+            <div>
+              {row[column.key]}
+              <i-button on-click={this.handleClick.bind(this, { row, index, column })}>编辑</i-button>
+            </div>
+          )
+        }
+        return item
+      } else return item
+    })
+    this.insideColumns = insideColumns
+  },
+  methods: {
+    handleClick ({ row, index, column }) {
+      console.log({ row, index, column })
+    }
+  }
+}
+``
+
 2）单个单元格编辑表格
 3）多个单元格编辑表格
