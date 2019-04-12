ec3d0f981b591cf4854d664e609ee9c26ab7565c
marlonchiu <Knight1898@outlook.com>
2019-04-12 20:51:47 +0800

    foler-tree 基础实现

--- src/views/folder-tree/folder-tree.vue
+++ src/views/folder-tree/folder-tree.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="folder-wrapper">
-    <Tree :data="folderTree"></Tree>
+    <Tree :data="folderTree" :render="renderFunc"></Tree>
   </div>
 </template>
 <script>
@@ -11,7 +11,17 @@ export default {
     return {
       folderList: [],
       fileList: [],
-      folderTree: []
+      folderTree: [],
+      // 自定义渲染
+      renderFunc: (h, { root, node, data }) => {
+        // console.log(data)
+        return (
+          <div class="tree-item">
+            { data.type === 'folder' ? <icon type="ios-folder" style="margin-right: 10px;"/> : ''}
+            { data.title }
+          </div>
+        )
+      }
     }
   },
   mounted () {
@@ -23,11 +33,11 @@ export default {
     // })
     // 让两个请求同时执行
     Promise.all([getFolderList(), getFileList()]).then(res => {
-      console.log(res)
-      let data1 = putFileInFolder(res[0], res[1])
-      console.log(data1)
-      let data2 = transferFolderToTree(data1)
-      console.log(data2)
+      // console.log(res)
+      // let data1 = putFileInFolder(res[0], res[1])
+      // console.log(data1)
+      // let data2 = transferFolderToTree(data1)
+      // console.log(data2)
       this.folderTree = transferFolderToTree(putFileInFolder(res[0], res[1]))
       // this.folderList = res[0]
       // this.fileList = res[1]
@@ -38,5 +48,11 @@ export default {
 <style lang="less">
 .folder-wrapper{
   width: 300px;
+  .tree-item{
+    width: ~"calc(100% - 50px)";
+    display: inline-block;
+    height: 30px;
+    line-height: 30px;
+  }
 }
 </style>
--- study_md/chapter17-19.md
+++ study_md/chapter17-19.md
@@ -364,3 +364,52 @@ export const transferFolderToTree = (folderList) => {
 ```
 
 3）自定义组件结构
+
+```vue
+<template>
+  <div class="folder-wrapper">
+    <Tree :data="folderTree" :render="renderFunc"></Tree>
+  </div>
+</template>
+<script>
+import { getFolderList, getFileList } from '@/api/data'
+import { putFileInFolder, transferFolderToTree } from '@/lib/util'
+export default {
+  data () {
+    return {
+      folderList: [],
+      fileList: [],
+      folderTree: [],
+      // 自定义渲染
+      renderFunc: (h, { root, node, data }) => {
+        // console.log(data)
+        return (
+          <div class="tree-item">
+            { data.type === 'folder' ? <icon type="ios-folder" style="margin-right: 10px;"/> : ''}
+            { data.title }
+          </div>
+        )
+      }
+    }
+  },
+  mounted () {
+    // 让两个请求同时执行
+    Promise.all([getFolderList(), getFileList()]).then(res => {
+      this.folderTree = transferFolderToTree(putFileInFolder(res[0], res[1]))
+    })
+  }
+}
+</script>
+<style lang="less">
+.folder-wrapper{
+  width: 300px;
+  .tree-item{
+    width: ~"calc(100% - 50px)";
+    display: inline-block;
+    height: 30px;
+    line-height: 30px;
+  }
+}
+</style>
+
+```
